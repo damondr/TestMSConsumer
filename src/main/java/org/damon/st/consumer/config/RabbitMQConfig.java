19a23a837784939ffcec8e07e39d64043c520b0c
Damon <bobylevd@gmail.com>
2023-11-08 11:55:26 +0300

    Mapstruct

--- src/main/java/org/damon/st/consumer/config/RabbitMQConfig.java
+++ src/main/java/org/damon/st/consumer/config/RabbitMQConfig.java
@@ -4,6 +4,7 @@ import org.damon.st.consumer.dto.UserOperationDto;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.support.converter.DefaultClassMapper;
+import org.springframework.amqp.support.converter.DefaultJackson2JavaTypeMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -31,12 +32,14 @@ public class RabbitMQConfig {
     public Jackson2JsonMessageConverter messageConverter() {
 
         Jackson2JsonMessageConverter messageConverter = new Jackson2JsonMessageConverter();
-        DefaultClassMapper classMapper = new DefaultClassMapper();
+        DefaultJackson2JavaTypeMapper typeMapper = new DefaultJackson2JavaTypeMapper();
+        typeMapper.setTypePrecedence(DefaultJackson2JavaTypeMapper.TypePrecedence.INFERRED);
+        typeMapper.setTrustedPackages("*");
         Map<String, Class<?>> idClassMapping = new HashMap<>();
         idClassMapping.put(
                 "org.damon.st.producer.dto.UserOperationDto", UserOperationDto.class);
-        classMapper.setIdClassMapping(idClassMapping);
-        messageConverter.setClassMapper(classMapper);
+        typeMapper.setIdClassMapping(idClassMapping);
+        messageConverter.setClassMapper(typeMapper);
 
         return messageConverter;
     }
